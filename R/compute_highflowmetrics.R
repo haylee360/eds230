16109c223ac4d5efc0c51c48457f93b2fe4e2b80
haylee360 <hayleeoyler360@gmail.com>
2025-06-04 10:57:44 -0700

    Calibration assignment

---
+++ R/compute_highflowmetrics.R
@@ -0,0 +1,37 @@
+#' High flow metrics
+#'
+#' Compute percent error between observation and model
+#' @param  m  model estimates
+#' @param  o  observations
+#' @param  month month
+#' @param  day day
+#' @param  year year
+#' @param high_flow_months which to use default (May 5)
+#' @return annual_max_err, annual_max_corr, high_month_cor, high_month_err
+
+compute_lowflowmetrics <- function(m, o, month, day, year, wy, high_flow_months = 5) {
+  flow <- cbind.data.frame(m, o, month, day, year, wy)
+  
+  # Get maximum yearly values
+  tmp <- flow %>%
+    group_by(wy) %>%
+    summarize(maxo = max(o), maxm = max(m))
+
+  annual_max_err <- mean(tmp$maxm - tmp$maxo)
+  annual_max_cor <- cor(tmp$maxm, tmp$maxo)
+
+  # Get monthly values
+  tmp <- flow %>%
+    group_by(month, year) %>%
+    summarize(model = sum(m), obs = sum(o))
+  
+  # Extract May
+  high <- subset(tmp, month %in% high_flow_months)
+  high_month_err <- mean(high$model - high$obs)
+  high_month_cor <- cor(high$model, high$obs)
+
+  return(list(
+    annual_max_err = annual_max_err, annual_max_cor = annual_max_cor, high_month_err = high_month_err,
+    high_month_cor = high_month_cor
+  ))
+}
